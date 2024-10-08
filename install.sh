5eee35840212b25fcc7fb380f699c852658a8aea
NeverCopyMe <I4104.vn@gmail.com>
2024-10-08 13:32:58 +0700

    Fix some bug

--- install.sh
+++ install.sh
@@ -28,69 +28,101 @@ sudo tee /etc/sddm.conf > /dev/null << EOF
 Current=catppuccin-mocha
 EOF
 
+# Chạy script Grub
+sudo ./grub/install.sh
+
 # Cài đặt Oh My Zsh
+cat <<EOF
+██╗███╗   ██╗███████╗████████╗ █████╗ ██╗     ██╗         ███╗   ██╗██╗   ██╗██╗███╗   ███╗       ██████╗  ██████╗ ███████╗██╗       ███████╗███████╗██╗  ██╗
+██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗██║     ██║         ████╗  ██║██║   ██║██║████╗ ████║       ██╔══██╗██╔═══██╗██╔════╝██║       ╚══███╔╝██╔════╝██║  ██║
+██║██╔██╗ ██║███████╗   ██║   ███████║██║     ██║         ██╔██╗ ██║██║   ██║██║██╔████╔██║       ██████╔╝██║   ██║█████╗  ██║         ███╔╝ ███████╗███████║
+██║██║╚██╗██║╚════██║   ██║   ██╔══██║██║     ██║         ██║╚██╗██║╚██╗ ██╔╝██║██║╚██╔╝██║       ██╔══██╗██║   ██║██╔══╝  ██║        ███╔╝  ╚════██║██╔══██║
+██║██║ ╚████║███████║   ██║   ██║  ██║███████╗███████╗    ██║ ╚████║ ╚████╔╝ ██║██║ ╚═╝ ██║▄█╗    ██║  ██║╚██████╔╝██║     ██║▄█╗    ███████╗███████║██║  ██║
+╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝    ╚═╝  ╚═══╝  ╚═══╝  ╚═╝╚═╝     ╚═╝╚═╝    ╚═╝  ╚═╝ ╚═════╝ ╚═╝     ╚═╝╚═╝    ╚══════╝╚══════╝╚═╝  ╚═╝
+EOF
+
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
 # Cài đặt các plugin Zsh
-git clone https://github.com/zsh-users/zsh-autosuggestions.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+git clone https://github.com/zsh-users/zsh-autosuggestions.git "~/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
 
 # Sao chép file cấu hình Zsh
 if [ -f ~/.zshrc ]; then
     mv ~/.zshrc ~/.zshrc.backup  # Lưu bản sao cũ nếu có
 fi
 cp ./configs/zsh/.zshrc ~/
+cp -r ./configs/alacritty ~/.config
+cp -r ./configs/rofi ~/.config
+cp -r ./configs/nvim ~/.config
 
-# Tải lại cấu hình Zsh
-source ~/.zshrc
-
-# Cấu hình Alacritty
-mkdir -p ~/.config/alacritty
-cp -r ./configs/alacritty ~/.config/alacritty/
-
-# Cấu hình Rofi
-mkdir -p ~/.config/rofi
-cp -r ./configs/rofi ~/.config/rofi
-
-# Cấu hình Neovim
-mkdir -p ~/.config/nvim
-cp -r ./configs/nvim ~/.config/nvim
+cat <<EOF
+██╗███╗   ██╗███████╗████████╗ █████╗ ██╗     ██╗         ██╗  ██╗██╗   ██╗██████╗ ██████╗ ██╗      █████╗ ███╗   ██╗██████╗ 
+██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗██║     ██║         ██║  ██║╚██╗ ██╔╝██╔══██╗██╔══██╗██║     ██╔══██╗████╗  ██║██╔══██╗
+██║██╔██╗ ██║███████╗   ██║   ███████║██║     ██║         ███████║ ╚████╔╝ ██████╔╝██████╔╝██║     ███████║██╔██╗ ██║██║  ██║
+██║██║╚██╗██║╚════██║   ██║   ██╔══██║██║     ██║         ██╔══██║  ╚██╔╝  ██╔═══╝ ██╔══██╗██║     ██╔══██║██║╚██╗██║██║  ██║
+██║██║ ╚████║███████║   ██║   ██║  ██║███████╗███████╗    ██║  ██║   ██║   ██║     ██║  ██║███████╗██║  ██║██║ ╚████║██████╔╝
+╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝    ╚═╝  ╚═╝   ╚═╝   ╚═╝     ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝ 
+EOF
 
 # Cấu hình Hyprland
 rm -rf ~/.config/hypr
-mkdir -p ~/.config/hypr
-cp -r ./hypr ~/.config/hypr
+cp -r ./hypr ~/.config
 
-# Cài đặt các gói phần mềm từ AUR và chính thức
-yay -S --noconfirm swaybg nwg-look zenity thunar selectdefaultapplication visual-studio-code-bin cliphist font-manager ttf-jetbrains-mono ttf-jetbrains-mono-nerd ttf-win10
+mkdir ~/.config/ags
+cp -r ./HyprPanel ~/
+ln -s $(pwd)/HyprPanel ~/.config/ags
+
+cat <<EOF
+██╗███╗   ██╗███████╗████████╗ █████╗ ██╗     ██╗         ████████╗██╗  ██╗███████╗███╗   ███╗███████╗
+██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗██║     ██║         ╚══██╔══╝██║  ██║██╔════╝████╗ ████║██╔════╝
+██║██╔██╗ ██║███████╗   ██║   ███████║██║     ██║            ██║   ███████║█████╗  ██╔████╔██║█████╗  
+██║██║╚██╗██║╚════██║   ██║   ██╔══██║██║     ██║            ██║   ██╔══██║██╔══╝  ██║╚██╔╝██║██╔══╝  
+██║██║ ╚████║███████║   ██║   ██║  ██║███████╗███████╗       ██║   ██║  ██║███████╗██║ ╚═╝ ██║███████╗
+╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝       ╚═╝   ╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝╚══════╝
+EOF                                                                                                   
 
 # Cài đặt các themes và icons
 mkdir -p ~/.themes ~/.icons
-tar -xf ./icons/Bibata-Modern-Ice.tar.xz -C ~/.icons/
-tar -xf ./icons/Win10Sur.tar.xz -C ~/.icons/
+tar -xf ./themes/Bibata-Modern-Ice.tar.xz -C ~/.icons/
+tar -xf ./themes/Win10Sur.tar.xz -C ~/.icons/
 tar -xf ./themes/CatppuccinMocha.tar.gz -C ~/.themes/
 
 # Cấu hình GTK
 rm -rf ~/.config/gtk-3.0
 cp -r ./icons/gtk-3.0 ~/.config/
 
-# Cài đặt các gói phần mềm bổ sung
-sudo pacman -S --needed python ufw rsync docker docker-compose
+cat <<EOF
+██╗███╗   ██╗███████╗████████╗ █████╗ ██╗     ██╗         ██████╗  █████╗  ██████╗██╗  ██╗ █████╗  ██████╗ ███████╗███████╗
+██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗██║     ██║         ██╔══██╗██╔══██╗██╔════╝██║ ██╔╝██╔══██╗██╔════╝ ██╔════╝██╔════╝
+██║██╔██╗ ██║███████╗   ██║   ███████║██║     ██║         ██████╔╝███████║██║     █████╔╝ ███████║██║  ███╗█████╗  ███████╗
+██║██║╚██╗██║╚════██║   ██║   ██╔══██║██║     ██║         ██╔═══╝ ██╔══██║██║     ██╔═██╗ ██╔══██║██║   ██║██╔══╝  ╚════██║
+██║██║ ╚████║███████║   ██║   ██║  ██║███████╗███████╗    ██║     ██║  ██║╚██████╗██║  ██╗██║  ██║╚██████╔╝███████╗███████║
+╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝    ╚═╝     ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚══════╝
+EOF
+
+yay -S --noconfirm swaybg nwg-look zenity thunar selectdefaultapplication visual-studio-code-bin cliphist font-manager ttf-jetbrains-mono ttf-jetbrains-mono-nerd ttf-win10
 yay -S --noconfirm electron wsysmon-git aylurs-gtk-shell github-desktop obs-studio ffmpeg vlc cava tty-clock opera opera-ffmpeg-codes dropbox
-sudo pacman -S --needed libgtop bluez bluez-utils btop networkmanager dart-sass wl-clipboard brightnessctl python gnome-bluetooth-3.0 pacman-contrib power-profiles-daemon
 yay -S --noconfirm grimblast-git gpu-screen-recorder hyprpicker matugen-bin python-gpustat grimblast swappy
-
-# Cài đặt bun.sh
-curl -fsSL https://bun.sh/install | bash
-
-# Cài đặt và cấu hình các gói bổ sung
+sudo pacman -S --needed python ufw rsync docker docker-compose
+sudo pacman -S --needed libgtop bluez bluez-utils btop networkmanager dart-sass wl-clipboard brightnessctl python gnome-bluetooth-3.0 pacman-contrib power-profiles-daemon
 sudo pacman -S --needed blueman networkmanager network-manager-applet pavucontrol brightnessctl
 sudo pacman -S --needed typescript npm meson gjs gtk3 gtk-layer-shell gnome-bluetooth-3.0 upower gobject-introspection libdbusmenu-gtk3 libsoup3
 
+cat <<EOF
+██╗███╗   ██╗███████╗████████╗ █████╗ ██╗     ██╗         ██████╗ ██╗   ██╗███╗   ██╗
+██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗██║     ██║         ██╔══██╗██║   ██║████╗  ██║
+██║██╔██╗ ██║███████╗   ██║   ███████║██║     ██║         ██████╔╝██║   ██║██╔██╗ ██║
+██║██║╚██╗██║╚════██║   ██║   ██╔══██║██║     ██║         ██╔══██╗██║   ██║██║╚██╗██║
+██║██║ ╚████║███████║   ██║   ██║  ██║███████╗███████╗    ██████╔╝╚██████╔╝██║ ╚████║
+╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝    ╚═════╝  ╚═════╝ ╚═╝  ╚═══╝
+EOF
+# Cài đặt bun.sh
+curl -fsSL https://bun.sh/install | bash
+sudo ln -s ~/.bun/bin/bun /usr/bin/bun
+
 # Kích hoạt các dịch vụ cần thiết
 sudo systemctl enable --now NetworkManager
-sudo systemctl enable --now sddm
 sudo systemctl enable --now docker
+sudo systemctl enable --now sddm
 
-# Chạy script Grub
-sudo ./grub/install.sh

0d43501ab4029261368bf405a03e9e2598d48a4c
NeverCopyMe <I4104.vn@gmail.com>
2024-10-16 14:47:49 +0700

    Hot Fix

--- install.sh
+++ install.sh
@@ -4,20 +4,23 @@
 sudo pacman -Syu --noconfirm
 
 # Cài đặt git và base-devel nếu chưa có
-sudo pacman -S --needed git base-devel
+sudo pacman -S --needed git base-devel curl
 
 # Tải và cài đặt yay (AUR helper)
 git clone https://aur.archlinux.org/yay.git
-cd yay || exit 1  # Dừng script nếu không vào được thư mục yay
+cd yay || exit 1
 makepkg -si --noconfirm
-cd .. || exit 1  # Dừng script nếu không trở về thư mục gốc
-rm -rf yay  # Xóa thư mục yay sau khi cài đặt
+cd .. || exit 1 
+rm -rf yay
 
 # Cập nhật lại hệ thống với yay
 yay -Syu --noconfirm
 
 # Cài đặt các gói phần mềm cần thiết
-sudo pacman -S --needed zsh neovim git curl hyprland sddm swww fcitx5-qt fcitx5-im fcitx5-unikey unzip qt6-svg qt6-declarative
+sudo pacman -S --needed alacritty zsh neovim 
+sudo pacman -S --needed fcitx5-qt fcitx5-im fcitx5-unikey unzip qt6-svg qt6-declarative
+sudo pacman -S --needed hyprland sddm swww unzip tar neofetch
+sudo pacman -R --noconfirm wofi
 
 # Giải nén theme cho sddm
 sudo unzip -o ./configs/sddm/catppuccin-mocha.zip -d /usr/share/sddm/themes/
@@ -51,57 +54,50 @@ cp -r ./hypr ~/.config
 echo '
 
 
-██╗███╗   ██╗███████╗████████╗ █████╗ ██╗     ██╗         ████████╗██╗  ██╗███████╗███╗   ███╗███████╗
-██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗██║     ██║         ╚══██╔══╝██║  ██║██╔════╝████╗ ████║██╔════╝
-██║██╔██╗ ██║███████╗   ██║   ███████║██║     ██║            ██║   ███████║█████╗  ██╔████╔██║█████╗  
-██║██║╚██╗██║╚════██║   ██║   ██╔══██║██║     ██║            ██║   ██╔══██║██╔══╝  ██║╚██╔╝██║██╔══╝  
-██║██║ ╚████║███████║   ██║   ██║  ██║███████╗███████╗       ██║   ██║  ██║███████╗██║ ╚═╝ ██║███████╗
-╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝       ╚═╝   ╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝╚══════╝
-
-
-'
-
-# Cài đặt các themes và icons
-mkdir -p ~/.themes ~/.icons
-echo "Unzip Cursor Theme..."
-tar -xf ./themes/Bibata-Modern-Ice.tar.xz -C ~/.icons/
-
-echo "Unzip Icon Theme..."
-tar -xf ./themes/Win10Sur.tar.xz -C ~/.icons/
-
-echo "Unzip Gtk Theme..."
-tar -xf ./themes/CatppuccinMocha.tar.gz -C ~/.themes/
-
-echo "Loading GTK Config..."
-rm -rf ~/.config/gtk-3.0
-cp -r ./themes/gtk-3.0 ~/.config/
-nwg-look -a
-
-echo "Done!"
-
-
-
-echo '
-
-
 ██╗███╗   ██╗███████╗████████╗ █████╗ ██╗     ██╗         ██████╗  █████╗  ██████╗██╗  ██╗ █████╗  ██████╗ ███████╗███████╗
 ██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗██║     ██║         ██╔══██╗██╔══██╗██╔════╝██║ ██╔╝██╔══██╗██╔════╝ ██╔════╝██╔════╝
 ██║██╔██╗ ██║███████╗   ██║   ███████║██║     ██║         ██████╔╝███████║██║     █████╔╝ ███████║██║  ███╗█████╗  ███████╗
 ██║██║╚██╗██║╚════██║   ██║   ██╔══██║██║     ██║         ██╔═══╝ ██╔══██║██║     ██╔═██╗ ██╔══██║██║   ██║██╔══╝  ╚════██║
- ██║██║ ╚████║███████║   ██║   ██║  ██║███████╗███████╗    ██║     ██║  ██║╚██████╗██║  ██╗██║  ██║╚██████╔╝███████╗███████║
+██║██║ ╚████║███████║   ██║   ██║  ██║███████╗███████╗    ██║     ██║  ██║╚██████╗██║  ██╗██║  ██║╚██████╔╝███████╗███████║
 ╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝    ╚═╝     ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚══════╝
 
 
 '
 
-yay -S --noconfirm swaybg nwg-look zenity thunar vencord vesktop selectdefaultapplication visual-studio-code-bin cliphist font-manager ttf-jetbrains-mono ttf-jetbrains-mono-nerd
-yay -S --noconfirm pipewire pipewire-module-xrdp pipewire-full-ffmpeg-git pipewire-screenaudio-git pipewire-session-manager xdg-desktop-portal-hyprland
-yay -S --noconfirm electron wsysmon-git aylurs-gtk-shell github-desktop obs-studio ffmpeg vlc cava tty-clock opera opera-ffmpeg-codes
-yay -S --noconfirm grimblast-git gpu-screen-recorder hyprpicker matugen-bin python-gpustat grimblast swappy
 sudo pacman -S --needed python ufw rsync docker docker-compose
-sudo pacman -S --needed libgtop bluez bluez-utils btop networkmanager dart-sass wl-clipboard brightnessctl python gnome-bluetooth-3.0 pacman-contrib power-profiles-daemon
+sudo pacman -S --needed libgtop bluez bluez-utils networkmanager dart-sass wl-clipboard brightnessctl gnome-bluetooth-3.0 pacman-contrib power-profiles-daemon
 sudo pacman -S --needed blueman networkmanager network-manager-applet pavucontrol brightnessctl
 sudo pacman -S --needed typescript npm meson gjs gtk3 gtk-layer-shell gnome-bluetooth-3.0 upower gobject-introspection libdbusmenu-gtk3 libsoup3
+
+packages=(
+    swaybg nwg-look zenity thunar vencord vesktop selectdefaultapplication visual-studio-code-bin cliphist rofi
+    font-manager ttf-jetbrains-mono ttf-jetbrains-mono-nerd
+    pipewire pipewire-module-xrdp pipewire-screenaudio-git pipewire-session-manager xdg-desktop-portal-hyprland
+    aylurs-gtk-shell electron obs-studio ffmpeg vlc cava tty-clock opera opera-ffmpeg-codes
+    grimblast-git gpu-screen-recorder hyprpicker matugen-bin python-gpustat grimblast swappy btop peazip
+    github-desktop gnome-keyring
+)
+
+install_package() {
+    local package="$1"
+    manager="yay"
+
+    if $manager -S --noconfirm --needed "$package"; then
+        echo "$package has been installed."
+    else
+        echo "Trying to install $package..."
+        if $manager -S --noconfirm --needed "$package"; then
+            echo "$package has been installed."
+        else
+            echo "Failed to install $package, please install it manually."
+        fi
+    fi
+}
+
+for package in "${packages[@]}"; do
+    install_package "$package"
+done
+
 
 read -p "Do you want to install window font? (Bạn có muốn cài đặt font của window không?) [Y/n]: " answer
 answer=${answer:-y}
@@ -123,14 +119,12 @@ echo '
 
 '
 
-# Cài đặt bun.sh
 curl -fsSL https://bun.sh/install | bash
 sudo ln -s ~/.bun/bin/bun /usr/bin/bun
 
 rm -rf ~/HyprPanel
 git clone https://github.com/Jas-SinghFSU/HyprPanel.git ~/HyprPanel && ln -s ~/HyprPanel $HOME/.config/ags
 
-# Cài đặt Oh My Zsh
 echo '
 
 
@@ -143,28 +137,58 @@ echo '
 
 '
 
-read d -p "Now, I will install zsh. It will take quite a bit of time (Bây giờ tôi sẽ cài đặt zsh, nó sẽ tốn khá nhiều thời gian) [Enter]"
+# Thông báo và yêu cầu người dùng xác nhận trước khi cài đặt zsh
+read -p "Now, I will install zsh. It will take quite a bit of time. [Enter]"
 read -p "******* PLEASE READ THE FOLLOWING INSTRUCTIONS CAREFULLY ******* [Enter]"
-echo "  "
-read -p "- Press 'y' when asked if you want to set zsh as the default shell (Nhập y khi được hỏi có muốn đặt zsh là mặc định) [Enter]"
-read -p "- After the installion process is complete, it will automatically swith to zsh. Please enter 'exit' and press Enter to continue (Sau khi cài đặt hoàn tất sẽ tự động chuyển sang zsh, nhập lệnh exit rồi enter để tiếp tục) [Enter]"
-echo "  "
+echo
+echo "- Press 'y' when asked if you want to set zsh as the default shell."
+echo "- After the installation process is complete, it will automatically switch to zsh. PLEASE enter 'exit' and press Enter to continue."
+echo
 read -p "******* PLEASE READ THE FOLLOWING INSTRUCTIONS CAREFULLY ******* [Enter]"
 
+# Cài đặt Zsh và Oh My Zsh
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
 # Cài đặt các plugin Zsh
-git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+for plugin in zsh-autosuggestions zsh-syntax-highlighting; do
+    git clone "https://github.com/zsh-users/$plugin.git" ~/.oh-my-zsh/custom/plugins/$plugin
+done
 
 # Sao chép file cấu hình Zsh
 if [ -f ~/.zshrc ]; then
     mv ~/.zshrc ~/.zshrc.backup  # Lưu bản sao cũ nếu có
 fi
+
 cp ./configs/zsh/.zshrc ~/
 cp -r ./configs/alacritty ~/.config
 cp -r ./configs/rofi ~/.config
 cp -r ./configs/nvim ~/.config
+
+echo '
+
+
+██╗███╗   ██╗███████╗████████╗ █████╗ ██╗     ██╗         ████████╗██╗  ██╗███████╗███╗   ███╗███████╗
+██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗██║     ██║         ╚══██╔══╝██║  ██║██╔════╝████╗ ████║██╔════╝
+██║██╔██╗ ██║███████╗   ██║   ███████║██║     ██║            ██║   ███████║█████╗  ██╔████╔██║█████╗  
+██║██║╚██╗██║╚════██║   ██║   ██╔══██║██║     ██║            ██║   ██╔══██║██╔══╝  ██║╚██╔╝██║██╔══╝  
+██║██║ ╚████║███████║   ██║   ██║  ██║███████╗███████╗       ██║   ██║  ██║███████╗██║ ╚═╝ ██║███████╗
+╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝       ╚═╝   ╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝╚══════╝
+
+
+'
+mkdir -p ~/.themes ~/.icons
+
+tar -xf ./themes/Bibata-Modern-Ice.tar.xz -C ~/.icons/
+tar -xf ./themes/Win10Sur.tar.xz -C ~/.icons/
+tar -xf ./themes/CatppuccinMocha.tar.gz -C ~/.themes/
+
+if [ -d ~/.config/gtk-3.0 ]; then
+    rm -rf ~/.config/gtk-3.0
+fi
+
+cp -r ./themes/gtk-3.0 ~/.config/
+nwg-look -a
+echo "Done!"
 
 # Kích hoạt các dịch vụ cần thiết
 sudo systemctl enable --now NetworkManager
